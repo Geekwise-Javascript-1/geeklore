12e726684db136d35d9c1f154d0b440ad1c20e48
Shane Eckert <shane@Shanes-MacBook-Pro.local>
2016-08-15 18:19:44 -0700

    day 3 no 4

---
+++ day02-lesson_3/assignment.js
@@ -0,0 +1,24 @@
+/*
+Shane Eckert
+JS1 Day 03 assignment
+*/
+
+// Prompt user for what they would like to do. Store as userResp
+var userResp = prompt('What would you like to do?');
+// If the user clicks on cancel, we get NULL. So let's test for that and
+// alert and console log that they did so.
+if ( userResp == null){
+  alert('You canceled!');
+  console.log(userResp);
+// Could use || above to save coding, but I like having two cases and two different messages
+// if ( userResp == null || userResp ==""){
+// so let's test for no value and then ask for input not a blank submission
+// Can't wait until we are using forms and not alerts!
+} else if (userResp == ""){
+  alert('Why blank?');
+  console.log(userResp);
+// If all is well, then let's alert and log the user's response
+} else {
+  alert(userResp);
+  console.log(userResp);
+}
